cd6b1cec7e4ddb3236711f0151c1022a8f1f275a
Nitin <nsinghboss02@gmail.com>
2021-12-02 11:57:54 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,26 @@
+package com.codechef;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	printing pattern:
+	          *
+	         ***
+	        *****
+	       *******
+	      *********
+	     ***********
+	 */
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        for(int i=0;i<n;i++){
+        }
+
+
+
+
+
+    }
+}
