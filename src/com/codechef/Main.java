4ad8a718a5991126e3eb5301aa679a525ed1876a
Nitin <nsinghboss02@gmail.com>
2021-12-03 20:09:32 +0530

    pyramid pattern completed

--- src/com/codechef/Main.java
+++ src/com/codechef/Main.java
@@ -15,12 +15,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n;
         n = sc.nextInt();
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=(2*i-1);k++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
-
-
-
-
 
     }
 }
